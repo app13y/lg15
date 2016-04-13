2d26a5d93648d917d67410f8fdbe5e1f6129bd4e
Arseny Aprelev <arseny.aprelev@gmail.com>
2016-04-13 15:00:19 +0300

    Rolls back memory management in GOST tests
    Minor changes
        + Fallback to crude memory management no longer requires memory allocation for encryption and decryption operations

--- tests/gost.c
+++ tests/gost.c
@@ -29,8 +29,8 @@ int testKeyScheduling(void) {
         goto cleanup;
     }
 
-    memory_ = malloc(WorkspaceOfScheduleEncryptionRoundKeys);
-    if (!memory_) {
+    memory_ = malloc(WorkspaceOfScheduleRoundKeys);
+    if (!memory_ && WorkspaceOfScheduleRoundKeys) {
         ++numberOfFailedTests_;
         goto cleanup;
     }
@@ -47,7 +47,6 @@ cleanup:
 
 int testBlockEncryption(void) {
     int numberOfFailedTests_ = 0;
-    void *memory_ = NULL;
     const uint8_t roundKeys_[NumberOfRounds * BlockLengthInBytes] = {
             0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
             0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
@@ -67,17 +66,9 @@ int testBlockEncryption(void) {
             0x7f, 0x67, 0x9d, 0x90, 0xbe, 0xbc, 0x24, 0x30, 0x5a, 0x46, 0x8d, 0x42, 0xb9, 0xd4, 0xed, 0xcd,
     };
 
-    memory_ = malloc(WorkspaceOfEncryptBlock);
-    if (!memory_) {
-        ++numberOfFailedTests_;
-        goto cleanup;
-    }
-
-    encryptBlock(roundKeys_, block_, memory_);
+    encryptBlock(roundKeys_, block_);
     numberOfFailedTests_ += (memcmp(block_, expectedCiphertext_, BlockLengthInBytes) != 0);
 
-    cleanup:
-    free(memory_);
     return numberOfFailedTests_;
 }
 
@@ -104,14 +95,14 @@ int testBlockDecryption(void) {
         goto cleanup;
     }
 
-    memory_ = malloc(WorkspaceOfScheduleDecryptionRoundKeys);
-    if (!memory_) {
+    memory_ = malloc(WorkspaceOfScheduleRoundKeys);
+    if (!memory_ && WorkspaceOfScheduleRoundKeys) {
         ++numberOfFailedTests_;
         goto cleanup;
     }
 
     scheduleDecryptionRoundKeys(roundKeys_, secretKey_, memory_);
-    decryptBlock(roundKeys_, block_, memory_);
+    decryptBlock(roundKeys_, block_);
     numberOfFailedTests_ += (memcmp(block_, expectedPlaintext_, BlockLengthInBytes) != 0);
 
     cleanup:
